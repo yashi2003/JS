86fc7c9980d1128b79a107224f29bcc900fbd59a
yashi2003 <yashimittal76@gmail.com>
2025-05-31 08:21:22 +0530

    datatypes in javascript

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3)  // This will show an alert with the result of 3 + 3 but nodejs has different syntax
+
+// readability
+ // Data Types in JavaScript
+let name = "hitesh";
+let age = 20;
+let isLoggedIn = true;
+let state = null; // no value
+let user; // undefined, not initialized
+
+//number ==> 2**53
+//bigint
+//string => ""
+// //boolean => true/false
+// //null => no value // standalone value // empty value // type is object (this is a bug in JS)
+// //undefined => not initialized // type is undefined 
+// symbol => unique and immutable value --> figma
+//object => collection of key-value pairs
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object (this is a bug in JS)
